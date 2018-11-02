22227f2f389a5315469d1868c9fc8438efb8b54c
Keller <381796751@qq.com>
2018-11-02 13:26:24 +0800

    获取最新歌曲列表

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -6,7 +6,7 @@
 		},
 		show() {
 			this.$el.addClass('active')
-		},	
+		},
 		hide() {
 			this.$el.removeClass('active')
 		}
@@ -18,6 +18,8 @@
 			this.view.init()
 			this.model = model
 			this.bindEventHub()
+			this.loadModule1()
+			this.loadModule2()
 		},
 		bindEventHub() {
 			window.eventHub.on('selectTab', (tabName) => {
@@ -28,6 +30,22 @@
 					this.view.hide()
 				}
 			})
+		},
+		loadModule1() {
+			let script1 = document.createElement('script')
+			script1.src = './js/index/page-1-1.js'
+			document.body.appendChild(script1)
+			script1.onload = function () {
+				console.log('模块1加载完毕')
+			}
+		},
+		loadModule2() {
+			let script2 = document.createElement('script')
+			script2.src = './js/index/page-1-2.js'
+			document.body.appendChild(script2)
+			script2.onload = function () {
+				console.log('模块2加载完毕')
+			}
 		}
 	}
 	controller.init(view, model)
